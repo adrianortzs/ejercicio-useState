1d731e9e52fd6a5df3577d88c9fbf6e761eb5f86
adrianortzs <adrianortizsuarez@icloud.com>
2024-06-19 14:29:22 +0200

    terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
 import './App.css'
+import {useState} from 'react'
 
 function App() {
  //Aquí tu código
+ const [name, setName] = useState('Sofía')
+ const [newName, setNewName] = useState('')
+
+const changeName = (event) => {
+    event.preventDefault()
+    if (newName !== '') {
+        setName(newName)
+        setNewName('')
+    }
+}
+
+return (
+    <div>
+        <h2>Teacher Name: {name}</h2>
+        <form onSubmit={changeName}>
+            <input type='text' value={newName} placeholder='Add a new name' onChange={(e) => setNewName(e.target.value)}></input>
+            <button type='submit'>Add</button>
+        </form> 
+    </div>
+)
 }
 
 export default App
+
+{/* <ul>
+            <li><button onClick={() => changeName('Data')}>Data</button></li>
+            <li><button onClick={() => changeName('Reyes')}>Reyes</button></li>
+            <li><button onClick={() => changeName('Yolanda')}>Yolanda</button></li>
+        </ul> */}
